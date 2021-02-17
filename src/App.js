a4922a6e4bf4db12c7e3c3043d9f625ee46b0249
Victor-Davydenko <59482820+Victor-Davydenko@users.noreply.github.com>
2021-02-17 08:59:30 +0200

    hw

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Picker = ({pickerHandler}) => {
+    return (
+        <>
+            <div onClick={pickerHandler} id='Rectangle'></div>
+            <div onClick={pickerHandler} id='Triangle'></div>
+            <div onClick={pickerHandler} id='Diamond'></div>
+        </>
+    );
+};
+
+const PresentationComponent = ({figure:{figure,width,height}})=> {
+    return(
+        <div>
+           <span>At the moment</span>
+           <span className='figure-name-holder'>{figure ? figure:'nothing'}</span>
+            <span>is picked</span>
+            <p>Width is {width} px</p>
+            <p>Height is {height} px</p>
+        </div>
+    );
+};
+
+class App extends React.Component {
+    state = {
+        figure: '',
+        width: 0,
+        height:0
+    };
+
+    pickerHandler = (e)=>{
+        if(e.target.id === 'Rectangle'||e.target.id === 'Triangle') {
+            this.setState({
+                figure: e.target.id,
+                width : e.target.offsetWidth,
+                height: e.target.offsetHeight
+            })
+        }
+        else if(e.target.id === 'Diamond') {
+            const width = Math.round(Math.sqrt(Math.pow(e.target.clientWidth,2)+ Math.pow(e.target.clientHeight,2)));
+            const height = width;
+            this.setState({
+                figure: e.target.id,
+                width,
+                height
+            })
+        }
+    };
+    render() {
+        return (
+            <div className="App">
+                <Picker pickerHandler={this.pickerHandler} />
+                <PresentationComponent figure={this.state} />
+            </div>
+        );
+    }
 }
 
 export default App;
